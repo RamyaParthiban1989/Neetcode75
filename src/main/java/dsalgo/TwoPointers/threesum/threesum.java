6f31f9d295e050dc57085a8d7f7c438074dbd7b4
R0P00YX <ramya.parthiban@walmart.com>
2025-08-19 07:48:41 +0530

    Create threesum.java

---
+++ src/main/java/dsalgo/TwoPointers/threesum/threesum.java
@@ -0,0 +1,18 @@
+package dsalgo.TwoPointers.threesum;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class threesum {
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] nums ={-1,0,1,2,-1,-4};
+        threeSum(nums);
+    }
+}
